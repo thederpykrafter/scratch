f4bb4f3fb75dfa737c2a415ebe8efc93f45a15b9
thederpykrafter <thederpykrafter@gmail.com>
2025-01-20 16:42:52 -0500

    init

---
+++ build.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // compile step
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "hello",
+        .root_source_file = b.path("main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(exe);
+    // Run step
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the program");
+    run_step.dependOn(&run_exe.step);
+}
