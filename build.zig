3e7a42580a2d9c4dcdd5bb48b7260b11a1fb9016
thederpykrafter <thederpykrafter@gmail.com>
2025-01-20 16:45:45 -0500

    correct source file name

--- build.zig
+++ build.zig
@@ -10,8 +10,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
     b.installArtifact(exe);
+
     // Run step
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the program");
